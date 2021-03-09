f953dd421357c2a2c0529c0fc6f30f5612e8b97c
higorvital <h.vitall96@gmail.com>
2021-03-09 19:29:06 -0300

    criar conta

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,21 +1,26 @@
 import React, { useCallback, useRef } from 'react';
 import Input from '../../components/Input';
-import {FiLock, FiMail} from 'react-icons/fi';
+import {FiLock, FiMail, FiUser} from 'react-icons/fi';
 import {Form} from '@unform/web';
 import {FormHandles} from '@unform/core';
 import * as Yup from 'yup';
 
 import { Container, Content, Background, ContentBox, AnimatedContent} from './styles';
 import Button from '../../components/Button';
+import api from '../../services/api';
+import { useHistory } from 'react-router';
 
 interface SignUpCredentials{
+  name: string;
   email: string;
   password: string;
+  password_confirmation: string;
 }
 
 const SignUp: React.FC = () => {
 
   const formRef = useRef<FormHandles>(null);
+  const history = useHistory();
 
   const handleSubmit = useCallback(async (data: SignUpCredentials)=>{
 
@@ -24,6 +29,7 @@ const SignUp: React.FC = () => {
     try {
 
       const schema = Yup.object().shape({
+        name: Yup.string().required(),
         email: Yup.string().email().required(),
         password: Yup.string().required(),
         password_confirmation: Yup.string().required().oneOf([Yup.ref('password')])
@@ -32,6 +38,10 @@ const SignUp: React.FC = () => {
       await schema.validate(data, {
         abortEarly: true
       });
+
+      await api.post('/users', data);
+
+      history.push('/');
 
     } catch (err) {
 
@@ -44,7 +54,7 @@ const SignUp: React.FC = () => {
       }
     }
 
-  },[]);
+  },[history]);
 
   return (
       <Container>
@@ -55,9 +65,10 @@ const SignUp: React.FC = () => {
               <h1>Criar sua conta</h1>
 
               <Form ref={formRef} onSubmit={handleSubmit}>
+                <Input icon={FiUser} type="name" name="name" placeholder="Nome" />
                 <Input icon={FiMail} type="email" name="email" placeholder="E-mail" />
                 <Input icon={FiLock} type="password" name="password" placeholder="Senha" />
-                <Input icon={FiLock} type="password" name="password_confirmation" placeholder="Senha" />
+                <Input icon={FiLock} type="password" name="password_confirmation" placeholder="Confirmação da senha" />
                 <Button>
                   Cadastrar
                 </Button>
